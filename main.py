82db9d0cb3163f4ff77881c2b1fdf94650609d79
PavelUkilevich <ukilevich@yandex.eu>
2021-12-14 23:11:28 +0300

    Капучино

--- main.py
+++ main.py
@@ -11,8 +11,18 @@ class MyWidget(QMainWindow):
         uic.loadUi("main.ui", self)
         self.con = sqlite3.connect("coffee.sqlite")
         self.pushButton.clicked.connect(self.update_result)
+        self.pushButton_2.clicked.connect(self.addS)
+        self.pushButton_3.clicked.connect(self.editS)
         self.modified = {}
         self.titles = None
+
+    def addS(self):
+        self.form = Addedit('add')
+        self.form.show()
+
+    def editS(self):
+        self.form = Addedit('edit')
+        self.form.show()
 
     def update_result(self):
         try:
@@ -42,8 +52,71 @@ class MyWidget(QMainWindow):
         pass
 
 
+class Addedit(QMainWindow):
+    def __init__(self, what):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        if what == 'add':
+            self.pushButton.setEnabled(True)
+            self.pushButton.clicked.connect(self.add_results)
+        else:
+            self.spinBox_2.setEnabled(True)
+            self.pushButton_2.setEnabled(True)
+            self.pushButton_2.clicked.connect(self.save_results)
+
+    def add_results(self):
+        cur = self.con.cursor()
+        lisedit = [self.lineEdit_2.text(), self.lineEdit_3.text(), self.lineEdit_4.text()]
+        if '' in lisedit:
+            self.label_8.setText('поле пусто')
+            return
+        else:
+            self.label_8.setText('')
+        if self.radioButton.isChecked():
+            checker = 'молотый'
+        elif self.radioButton_2.isChecked():
+            checker = 'в зёрнах'
+        cur.execute("""INSERT INTO coffe(название_сорта, степень_обжарки, молотый_в_зернах, описание_вкуса,
+                                                              цена, объем_упаковки) VALUES(?, ?, ?, ?, ?, ?)""",\
+                    (lisedit[0], lisedit[1], checker, lisedit[2], self.spinBox.value(), self.spinBox_3.value()))
+        self.con.commit()
+        self.close()
+
+    def save_results(self):
+        cur = self.con.cursor()
+        ids = cur.execute("SELECT ID FROM coffe").fetchall()
+        lis = []
+        for i in ids:
+            lis.append(i[0])
+        if self.spinBox_2.value() not in lis:
+            self.label_8.setText('неправильный ID')
+            return
+        else:
+            self.label_8.setText('')
+        lisedit = [self.lineEdit_2.text(), self.lineEdit_3.text(), self.lineEdit_4.text()]
+        if '' in lisedit:
+            self.label_8.setText('поле пусто')
+            return
+        else:
+            self.label_8.setText('')
+        if self.radioButton.isChecked():
+            checker = 'молотый'
+        elif self.radioButton_2.isChecked():
+            checker = 'в зёрнах'
+
+        que = """UPDATE coffe
+                                  SET название_сорта = ?, степень_обжарки = ?, молотый_в_зернах = ?, описание_вкуса = ?,
+                                  цена = ?, объем_упаковки = ?              
+                                  WHERE ID = ?"""
+        cur.execute(que, (lisedit[0], lisedit[1], checker, lisedit[2], self.spinBox.value(), self.spinBox_3.value(),
+                          self.spinBox_2.value()))
+        self.con.commit()
+        self.close()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
